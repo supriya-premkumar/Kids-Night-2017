80737819fdbc703a0599aee44acca9e1d96702f5
supriya-premkumar <supriya@fastmail.com>
2017-10-14 12:48:31 -0700

    Integrated dial

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 function openWindow(){
-  window.open(document.location.href="../html/main.html");
+  window.open("../html/output.html");
   // myWindow=window.open('','','width=200,height=100');
   //           myWindow.document.write("<p>This is 'myWindow'</p>");
   //           myWindow.focus();
 }
+
+var dial = $(".dial .inner");
+	var gauge_value = $(".gauge .value");
+
+		function rotateDial()
+		{
+			var deg = 0;
+			var value = Math.round(Math.random()*100);
+			deg = (value * 177.5) / 100;
+
+			gauge_value.html(value + "%");
+
+			dial.css({'transform': 'rotate('+deg+'deg)'});
+		    dial.css({'-ms-transform': 'rotate('+deg+'deg)'});
+		    dial.css({'-moz-transform': 'rotate('+deg+'deg)'});
+		    dial.css({'-o-transform': 'rotate('+deg+'deg)'});
+		    dial.css({'-webkit-transform': 'rotate('+deg+'deg)'});
+		}
+
+		setInterval(rotateDial, 2000);
