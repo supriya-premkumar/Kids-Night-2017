05b8af9749c7666fed5c26844d244da95680e05d
supriya-premkumar <supriya@fastmail.com>
2017-10-14 11:49:53 -0700

    Image Styling and Start button

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function openWindow(){
+  window.open(document.location.href="../html/main.html");
+  // myWindow=window.open('','','width=200,height=100');
+  //           myWindow.document.write("<p>This is 'myWindow'</p>");
+  //           myWindow.focus();
+}
